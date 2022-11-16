e3798124087f87138361942e33d24752b4f80b07
Saman Mehr Ali Pur <saman_mehr_ali_poor@tutanota.com>
2022-11-16 18:38:51 +0330

    init commit

---
+++ tree.py
@@ -0,0 +1,19 @@
+
+
+from traceback import print_tb
+from turtle import left, right
+
+
+class Node:
+    def __init__(self):
+        self.data=None
+        self.root=None
+        self.rightChild=None
+        self.leftChild=None
+
+
+    def addNode(self,newNode):
+        
+        
+       
+        
